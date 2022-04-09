d1911bd2d122b90ba57ac104f1daf60047284d7f
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-04-10 01:30:11 +0200

    update server address

--- client_macro/src/hooks/server.ts
+++ client_macro/src/hooks/server.ts
@@ -6,6 +6,6 @@ import CounterAbi from '~/abi/server_abi.json'
 export function useServerContract() {
   return useContract({
     abi: CounterAbi as Abi,
-    address: '0x05d5ee760639052254c2b94c7eb6d8c859ea16cdbc57063be52a872340920ef5',
+    address: '0x07f06feaa05a72ea4b5dcac5f6e959660400bc04c35ac1a6279a5d8e321b6c7c',
   })
 }
