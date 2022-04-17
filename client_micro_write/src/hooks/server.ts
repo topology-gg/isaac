cec6b4f811da4bf249ac40f29d5d8c0c64d7d5ff
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-04-17 22:34:56 +0200

    update server address

--- client_micro_write/src/hooks/server.ts
+++ client_micro_write/src/hooks/server.ts
@@ -6,6 +6,6 @@ import ServerAbi from '~/abi/server_abi.json'
 export function useServerContract() {
   return useContract({
     abi: ServerAbi as Abi,
-    address: '0x06a98d9f4b77dd225569065f7c0eea2b93eff6dcc35b2780ca9613425cbbe62a',
+    address: '0x0025ecf8ef3993263fec37a54dd730c5d10fa347d1427c584de0a48ec292b4b4',
   })
 }
