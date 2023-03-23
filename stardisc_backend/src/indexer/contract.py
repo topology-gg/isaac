a1fda5c5e964528d499c45016a4f6cbdf317ef71
Gregory Edison <gregory.edison1993@gmail.com>
2023-03-23 11:46:33 +0100

    feat: update indexer

--- stardisc_backend/src/indexer/contract.py
+++ stardisc_backend/src/indexer/contract.py
@@ -1,7 +1,7 @@
 from typing import Any, Iterator, Tuple
 
-from apibara.model import Event
-
+from apibara.starknet import FieldElement, felt
+from apibara.starknet.proto.starknet_pb2 import Event
 
 STARK_PRIME = (
     3618502788666131213697322783095070105623107215331596699973092056135872020481
@@ -10,29 +10,20 @@ STARK_PRIME_HALF = (
     1809251394333065606848661391547535052811553607665798349986546028067936010240
 )
 
-def _felt_from_iter (it: Iterator[bytes], signed=True):
-    fe = int.from_bytes(next(it), "big")
 
-    if signed:
-        if fe > STARK_PRIME_HALF:
-            fe = fe - STARK_PRIME
+# Convert a FieldElement to an int.
+def int_from_felt(it: Iterator[FieldElement]):
+    i = felt.to_int(next(it))
+    return i
 
-    return fe
 
 #
 # Decode event: new_puzzle_occurred
 #
-def decode_sns_register_occurred (event: Event) -> Tuple[int, int]:
+def decode_sns_register_occurred(event: Event) -> Tuple[int, int]:
     it = iter(event.data)
 
-    # @event
-    # func sns_register_occurred (
-    #         addr : felt,
-    #         name : felt
-    #     ):
-    # end
-
-    addr = _felt_from_iter (it, signed=False)
-    name = _felt_from_iter (it, signed=False)
+    addr = int_from_felt(it)
+    name = int_from_felt(it)
 
     return addr, name
